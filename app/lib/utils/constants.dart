675ecf4971bb490355e49e54050b8182850e1065
Pushpendra Suryawanshi <pushpendrasuryawanshi3@gmail.com>
2026-04-04 11:34:31 +0530

    feat(mobile): initialize project core and foundational data models

---
+++ app/lib/utils/constants.dart
@@ -0,0 +1,12 @@
+class AppConstants {
+  static const String appName = 'UrbanEye';
+  
+  // Storage/Pref Keys
+  static const String userTokenKey = 'USER_TOKEN';
+  static const String userRoleKey = 'USER_ROLE';
+  
+  // UI Constants for standard metrics
+  static const double borderRadius = 16.0;
+  static const double cardElevation = 8.0;
+  static const double defaultPadding = 24.0;
+}
